6fb36467d33a04f53e895ca632babb81e43f9eb2
yhyddr <abserari@gmail.com>
2020-07-04 13:18:09 +0800

    complete rustlings

---
+++ exercises/clippy/target/debug/deps/clippy2-3254ac67ef0835eb.d
@@ -0,0 +1,5 @@
+/Users/abser/rustlings/exercises/clippy/target/debug/deps/clippy2-3254ac67ef0835eb.rmeta: clippy2.rs
+
+/Users/abser/rustlings/exercises/clippy/target/debug/deps/clippy2-3254ac67ef0835eb.d: clippy2.rs
+
+clippy2.rs:
